259f41c1976ffe9ec42b02fe0f9756817985f5aa
yukino99 <74867945+yukino99@users.noreply.github.com>
2021-02-01 15:13:39 +0530

    Update settings-sample.php

--- settings-sample.php
+++ settings-sample.php
@@ -1,6 +1,6 @@
 <?php
-define("BOT_ID",""); //Your telegram bot ID
-define("ADMIN_ID",""); //Your telegram ID
+define("BOT_ID","1690117632:AAGFCKmKAAKQyEVHHZtSLyTeOOLaFD5D35g"); //Your telegram bot ID
+define("ADMIN_ID","1377113931"); //Your telegram ID
 define("CHANNEL_ID",""); //Your telegram channel ID
 define("CAPTION",""); //The caption to add to any content
 define("WHITELIST",array(ADMIN_ID,CHANNEL_ID)); //Add here more users to the whitelist
